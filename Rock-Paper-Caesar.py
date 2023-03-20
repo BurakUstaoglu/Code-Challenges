5f5c546e96f24f747f79b7111ebbcfc76e19d869
BurakUstaoglu <121027106+BurakUstaoglu@users.noreply.github.com>
2023-03-20 09:41:24 +0300

    RPC

--- Rock-Paper-Caesar.py
+++ Rock-Paper-Caesar.py
@@ -152,7 +152,6 @@ def GameMechanic():
                 bResult = []
                 for i in range(len(bRPC) // 2):
                     bResult.append((bRPC.index(bInput) + (i + 1)) % len(bRPC) )
-                    #print(bResult)
 
                 if x in bResult:
                     ScoreManager(0)
